9fb4502bd140936c302ac721a0754261a1d0ea25
Ricardo Rocha <me@rrocha.uk>
2024-09-14 23:10:16 +0100

    add api rate-limiting

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -16,9 +16,28 @@ export default defineNuxtConfig({
   modules: [
     '@pinia/nuxt',
     'nuxt-cron',
+    'nuxt-api-shield',
   ],
   cron: {
     timeZone: 'Europe/London',
     jobsDir: 'cron',
+  },
+  nuxtApiShield: {
+    limit: {
+      max: 30,
+      duration: 60 * 30,
+      ban: 3600,
+    },
+    log: {
+      path: "Logs",
+      attempts: 100,
+    },
+  },
+  nitro: {
+    storage: {
+      'shield': {
+        driver: 'memory',
+      },
+    },
   },
 });
