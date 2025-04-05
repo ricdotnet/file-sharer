6d7d2a69b11f3588d40bbfee312abe716057e2d6
Ricardo Rocha <me@rrocha.uk>
2025-04-05 12:25:43 +0100

    fix api/files/[id/fileName] bugs

--- server/api/files/[id].get.ts
+++ server/api/files/[id].get.ts
@@ -2,7 +2,7 @@ import { H3Event } from 'h3';
 import { findFileByFilename } from '~/server/utils/db';
 
 export default defineEventHandler(async (event: H3Event) => {
-  const fileName = getRouterParam(event, 'fileName');
+  const fileName = getRouterParam(event, 'id');
 
   if (!fileName) {
     return;
