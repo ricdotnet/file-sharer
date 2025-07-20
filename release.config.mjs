26906ee8a20e9afad7aac73cccff44d6b024b06d
Ricardo Rocha <me@rrocha.uk>
2025-07-20 11:52:28 +0100

    chore: add action for tag and use tag for versioning

---
+++ release.config.mjs
@@ -0,0 +1,20 @@
+/**
+ * @type {import('semantic-release').GlobalConfig}
+ */
+export default {
+  branches: ['main'],
+  plugins: [
+    [
+      '@semantic-release/commit-analyzer',
+      {
+        releaseRules: [
+          { type: 'feat', release: 'minor' },
+          { type: 'fix', release: 'patch' },
+          { type: 'chore', release: 'patch' },
+          { type: 'patch', release: 'patch' },
+        ],
+      },
+    ],
+    '@semantic-release/github',
+  ],
+};
